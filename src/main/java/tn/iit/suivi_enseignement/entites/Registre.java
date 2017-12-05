08cb43b52e271918c6586e6d622b423ae9ed818b
trabelsi-morsi <trabelsi.morsi@gmail.com>
2017-12-05 18:33:05 +0100

    service registre

---
+++ src/main/java/tn/iit/suivi_enseignement/entites/Registre.java
@@ -0,0 +1,88 @@
+package tn.iit.suivi_enseignement.entites;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Registre implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private String email;
+	@Column(name = "datedenaissance")
+	private String dateNaissance;
+	@Column
+	private String password;
+	@Column(name = "username")
+	private String userName;
+
+	public Registre() {
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDateNaissance() {
+		return dateNaissance;
+	}
+
+	public void setDateNaissance(String dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Registre other = (Registre) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Registre [email=" + email + ", dateNaissance=" + dateNaissance + ", password=" + password
+				+ ", userName=" + userName + "]";
+	}
+
+}
